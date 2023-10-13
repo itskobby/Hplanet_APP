61d76d66be5acec7637605ba314b592196e064f8
its_kobby <emmanuel.adjei-asumah@st.gimpa.edu.gh>
2023-10-13 13:43:42 +0100

    first commit

---
+++ index.mjs
@@ -0,0 +1,47 @@
+'use strict'
+let log=console.log;
+
+//requiring the csv-parse package downloaded
+import {parse} from 'csv-parse';
+// requiring the file system from node modules
+import fs from 'fs';
+
+// creating an array to store incoming data
+const habitablePlanets=[];
+
+
+// creating a funtion to find habitable planets
+function isHabittablePlanet(planet){
+    return planet['koi_disposition']==='CONFIRMED'
+    && planet['koi_insol']>0.36 && planet['koi_insol']<1.11
+   && planet['koi_prad']>1.6;   
+};
+
+// passing in the kepler_data file
+fs.createReadStream('kepler_data1.csv')
+/* calling the .pipe function to connect the readablestream source
+   to a writeablestream destination. the pipe function is on the csv-parse
+   pacakage
+*/
+.pipe(parse({
+    Comment: '#',
+    columns: true
+}))
+//this gives us access to fs event emiter which we can react to
+// reacting to the data event by placing the data in results array
+.on('data',(data)=>{
+    if(isHabittablePlanet(data)){
+        habitablePlanets.push(data)
+    };
+    
+})
+// listening for an error event and decribing the error
+.on('error',(err)=>{
+    log(err); 
+})
+// listenin for the end event, logging the results array
+.on('end',()=>{
+   log(`${habitablePlanets.length} habitable planets found.`);
+   log('done');
+});
+
