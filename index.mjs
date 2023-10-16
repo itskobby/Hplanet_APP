4d215a8779742a1060092a460f54bbc87397b467
its_kobby <emmanuel.adjei-asumah@st.gimpa.edu.gh>
2023-10-16 12:24:31 +0100

    second commit

--- index.mjs
+++ index.mjs
@@ -18,20 +18,20 @@ function isHabittablePlanet(planet){
 };
 
 // passing in the kepler_data file
-fs.createReadStream('kepler_data1.csv')
+fs.createReadStream('kepler_data.csv')
 /* calling the .pipe function to connect the readablestream source
    to a writeablestream destination. the pipe function is on the csv-parse
    pacakage
-*/
+*/ 
 .pipe(parse({
     Comment: '#',
-    columns: true
+    columns: true,
 }))
 //this gives us access to fs event emiter which we can react to
 // reacting to the data event by placing the data in results array
 .on('data',(data)=>{
     if(isHabittablePlanet(data)){
-        habitablePlanets.push(data)
+        habitablePlanets.push(data);
     };
     
 })
